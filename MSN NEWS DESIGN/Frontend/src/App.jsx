69f8c32accf2d72fd06d39f5b98fd9f132e8de90
Md Shahnewaz Akhtar <mdcodingkiduniya@gmail.com>
2025-07-02 19:59:49 -0700

    installing shadcn UI

--- MSN NEWS DESIGN/Frontend/src/App.jsx
+++ MSN NEWS DESIGN/Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div>
       <h2>hello</h2>
+      <Button className="bg-red-500" variant="ghost">add</Button>
     </div>
   )
 }
